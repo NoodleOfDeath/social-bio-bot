f581b986f3f5c236303f837fa867f26183e8c8c4
NoodleOfDeath <thom@noodleofdeath.com>
2022-04-10 17:14:55 -0400

    minor stdout updates

--- src/types/task.ts
+++ src/types/task.ts
@@ -18,18 +18,15 @@ export class IgCycleProfilePictureTask implements ITask<IDPSession<IgApiClient>>
     this.generate =
       generate ||
       ((session: IDPSession<IgApiClient>): Promise<void> => {
-        console.log('tits', dir);
         const images = readdirSync(dir);
-        console.log(images);
         return new Promise<void>((resolve, reject) => {
           if (images.length < 1) reject(new Error(`No images were found in ${dir}`));
           let n = Math.floor(Math.random() * images.length);
           while (n === this.index) n = Math.floor(Math.random() * images.length);
           this.index = n;
           const imgPath = join(dir, images[this.index]);
-          console.log(`attempting to read file ${imgPath}`);
+          console.log(`attempting to upload file ${images[this.index]}`);
           const buffer = readFileSync(imgPath);
-          console.log(buffer);
           session.client.account
             .changeProfilePicture(buffer)
             .then(() => {
